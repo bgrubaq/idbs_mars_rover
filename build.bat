cbf7f98355d0b8dd35d025dd9b1ca1ad290ff197
tegan <tegan@IDBSUSL005.idbs.co.uk>
2025-05-29 12:48:40 -0400

    updated bat to include pause and prevent nested clone if running in ide

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 @echo off
 if not exist "idbs_mars_rover" (
+  if not exist ".git" (
 	git clone https://github.com/bgrubaq/idbs_mars_rover
+  )
 )
 cd idbs_mars_rover
 git pull
 call mvn -f .\pom.xml package
 java -cp .\target\idbs_mars_rover-1.0-SNAPSHOT.jar com.codemanship.marsrover.Program
+pause
