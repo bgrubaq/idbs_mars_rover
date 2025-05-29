ffe04f11ab4bdea5647121ffdc7772ddd3ff7b84
BWake <BWake@IDBSUSL064.idbs.co.uk>
2025-05-29 11:57:40 -0400

    Added location to drop

--- src/main/java/com/codemanship/marsrover/MarsRover.java
+++ src/main/java/com/codemanship/marsrover/MarsRover.java
@@ -3,14 +3,22 @@ package com.codemanship.marsrover;
 public class MarsRover
 {
 	private String direction;
+	private int x, y;
 	
-	public MarsRover(String direction)
+	public MarsRover(String direction, int x, int y)
 	{
 		this.direction = direction;
+		this.x = x;
+		this.y = y;
 	}
 	
 	public String getDirection()
 	{
 		return direction;
+	}
+
+	public int[] getLocation()
+	{
+		return new int[] {x, y};
 	}
 }
--- src/test/java/com/codemanship/marsrover/TestMarsRover.java
+++ src/test/java/com/codemanship/marsrover/TestMarsRover.java
@@ -6,31 +6,35 @@ import org.junit.jupiter.api.Test;
 public class TestMarsRover
 {
 	@Test
-	public void testDropFacingNorth()
+	public void testDropFacingNorthAtLocation()
 	{
-		MarsRover rover = new MarsRover("North");
+		MarsRover rover = new MarsRover("North", 2, 2);
 		Assertions.assertEquals("North", rover.getDirection());
+		Assertions.assertArrayEquals(new int[] {2,2}, rover.getLocation());
 	}
 	
 	@Test
-	public void testDropFacingSouth()
+	public void testDropFacingSouthAtLocation()
 	{
-		MarsRover rover = new MarsRover("South");
+		MarsRover rover = new MarsRover("South", 4, 7);
 		Assertions.assertEquals("South", rover.getDirection());
+		Assertions.assertArrayEquals(new int[] {4, 7}, rover.getLocation());
 	}
 
 	@Test
 	public void testDropFacingWest()
 	{
-		MarsRover rover = new MarsRover("West");
+		MarsRover rover = new MarsRover("West", 6, 2);
 		Assertions.assertEquals("West", rover.getDirection());
+		Assertions.assertArrayEquals(new int[] {6, 2}, rover.getLocation());
 	}
 
 	@Test
 	public void testDropFacingEast()
 	{
-		MarsRover rover = new MarsRover("East");
+		MarsRover rover = new MarsRover("East", 35, 1);
 		Assertions.assertEquals("East", rover.getDirection());
+		Assertions.assertArrayEquals(new int[] {35, 1}, rover.getLocation());
 	}
 
 }
