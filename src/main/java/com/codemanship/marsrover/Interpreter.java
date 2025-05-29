48a8bc332e7845da1764fb82c7e8211cbb0c3e35
Ben Gruba <bgruba@quantrix.com>
2025-05-29 11:02:19 -0500

    Initial interpreter implementation

---
+++ src/main/java/com/codemanship/marsrover/Interpreter.java
@@ -0,0 +1,39 @@
+package com.codemanship.marsrover;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Interpreter<Target>
+{
+    private final Target target;
+
+    private final Map<String, Consumer<Target>> commands;
+
+    private final InputStream in;
+
+    public Interpreter(
+        Target target,
+        Map<String, Consumer<Target>> commands,
+        InputStream in
+    )
+    {
+        this.target = target;
+        this.commands = new HashMap<>(commands);
+        this.in = in;
+    }
+
+    public void run()
+    {
+        Scanner s = new Scanner(in);
+
+        while (s.hasNextLine()){
+            String input = s.nextLine();
+            Consumer<Target> cmd = commands.get(input);
+            if (cmd != null)
+                cmd.accept(target);
+        }
+    }
+}
---
+++ src/test/java/com/codemanship/marsrover/InterpreterTests.java
@@ -0,0 +1,37 @@
+package com.codemanship.marsrover;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.junit.jupiter.api.Test;
+
+public class InterpreterTests
+{
+    private interface TestTarget
+    {
+        void sampleOperation();
+    }
+
+    @Test
+    public void canRunCommand()
+    {
+        Map<String, Consumer<TestTarget>> commands = new HashMap<>();
+
+        commands.put("S", TestTarget::sampleOperation);
+
+        TestTarget module = mock(TestTarget.class);
+
+        InputStream in = new ByteArrayInputStream("S\n".getBytes());
+
+        new Interpreter<>(module, commands, in).run();
+
+        verify(module).sampleOperation();
+    }
+}
